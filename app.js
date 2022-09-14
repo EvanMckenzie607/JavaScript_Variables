e12608b32164a25206f59a4997a500fc29be42cd
Evan Mckenzie <615evan@gmail.com>
2022-09-14 15:53:55 -0400

    Done

--- app.js
+++ app.js
@@ -4,7 +4,38 @@ console.log(
 );
 
 // Exercise 1
+const firstname= "Evan";
+let lastname;
+var age;
 
 // Exercise 2
+lastname = "Mckenzie";
+age = 26;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
+
+let product = " Screaming Rubber Chicken";
+let price = 19.95;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let instock = false;
+let selectedSize = "M";
+
+console.log(`${product}\n In size ${selectedSize} is ${price} with ${stock}
+ in stock`);
+
+ let title = "Name of the Wind";
+ let author = "Patrick Rothfuss"
+ let pageCount = 772;
+ let bookmark = 456;
+ let hasRead = true;
+
+console.log(` The book ${title}, \n Written by ${author}\n Is ${pageCount} pages long,\n
+Your last bookmark was on page ${bookmark}`);
